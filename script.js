583a37582337a65107d81f67ef34c312a921ff13
flyspotbar <flyspotbar@gmail.com>
2026-07-08 18:20:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -39,30 +39,70 @@ document.querySelectorAll("section,.item").forEach(el=>{
 const translations = {
 
 pl: {
+"Double Espresso":"Podwójne Espresso",
+"Americano":"Americano",
+"Cappuccino":"Cappuccino",
+"Latte":"Latte",
+"Flat White":"Flat White",
+"Ice Americano":"Americano z lodem",
+"Ice Cappuccino":"Cappuccino z lodem",
+"Ice Latte":"Latte z lodem",
 
-"Coffee":"Kawa",
-"Breakfast":"Śniadania",
-"Main Dishes":"Dania główne",
-"Soups":"Zupy",
-"Salads":"Sałatki",
-"Drinks":"Napoje",
+"Caramel / Vanilla / Chocolate / Strawberry syrup":"Syrop karmelowy / waniliowy / czekoladowy / truskawkowy",
+"Lactose Free Milk":"Mleko bez laktozy",
+"Oat / Coconut / Almond Milk":"Mleko owsiane / kokosowe / migdałowe",
 
-"Chicken Broth":"Rosół",
-"Ukrainian Borscht":"Barszcz ukraiński",
+"English Breakfast + Coffee":"Śniadanie angielskie + kawa",
+"Toast with salami & cheese":"Tost z salami i serem",
+"Syrniki with topping":"Syrniki z dodatkiem",
+"Avocado toast with smoked salmon & poached egg":"Tost z awokado, wędzonym łososiem i jajkiem w koszulce",
 
-"Tea":"Herbata",
-"Orange Juice":"Sok pomarańczowy",
-"Apple Juice":"Sok jabłkowy",
+"Minced Meat Cutlet":"Kotlet mielony",
+"Hungarian Potato Pancakes":"Placki ziemniaczane po węgiersku",
+"Chicken & Mushroom Crepes":"Naleśniki z kurczakiem i pieczarkami",
+"Spaghetti Bolognese":"Spaghetti Bolognese",
+    "Chicken Cutlet":"Kotlet z kurczaka",
 
-"Scrambled eggs (3 eggs)":"Jajecznica (3 jajka)",
-"Scrambled eggs with bacon":"Jajecznica z boczkiem",
-"English Breakfast":"Śniadanie angielskie",
+"Side dish: potatoes / fries / rice / buckwheat":"Dodatek: ziemniaki / frytki / ryż / kasza",
+"Included":"W cenie",
 
-"Chicken Cutlet":"Kotlet drobiowy",
-"Pork Cutlet":"Kotlet schabowy",
+"Chicken Salad":"Sałatka z kurczakiem",
+"Shrimp Salad":"Sałatka z krewetkami",
+"Vegetarian Salad":"Sałatka wegetariańska",
 
-"Bistro & Catering":"Bistro i Catering"
+"Extra Bacon":"Dodatkowy boczek",
+"Hot Dog with Bacon & Fries":"Hot Dog z boczkiem i frytkami",
+"Traditional Zapiekanka":"Tradycyjna zapiekanka",
+"Ham Zapiekanka":"Zapiekanka z szynką",
+"Gyros Zapiekanka":"Zapiekanka Gyros",
+"Chicken Nuggets (6 pcs)":"Nuggetsy z kurczaka (6 szt.)",
+"Nuggets + Fries":"Nuggetsy + frytki",
+"Chicken Strips (5 pcs)":"Stripsy z kurczaka (5 szt.)",
+"Strips + Fries":"Stripsy + frytki",
+"Cheese Sticks (6 pcs)":"Paluszki serowe (6 szt.)",
+"Cheese Sticks + Fries":"Paluszki serowe + frytki",
+"Chicken Wings (6 pcs)":"Skrzydełka z kurczaka (6 szt.)",
+"Chicken Wings + Fries":"Skrzydełka + frytki",
 
+"Sauces: Ketchup, Mayo, Mustard, BBQ, 1000 Islands, Curry, American, Garlic":"Sosy: Ketchup, Majonez, Musztarda, BBQ, 1000 Islands, Curry, American, Czosnkowy",
+
+"Virgin Mojito":"Virgin Mojito",
+"Apple Mint":"Jabłko Mięta",
+
+"Honey & Cocoa Marlenka":"Marlenka miodowo-kakaowa",
+"Jelly Dessert":"Deser galaretkowy",
+
+"Fresh Orange Juice":"Świeży sok pomarańczowy",
+"Lipton Ice Tea":"Lipton Ice Tea",
+"Oshee Isotonic":"Oshee Isotonic",
+"Oshee Vitamin":"Oshee Vitamin",
+"Aqua Water":"Woda Aqua",
+"King Water":"Woda King",
+"Pepsi / Mirinda / 7UP":"Pepsi / Mirinda / 7UP",
+"Pepsi Can":"Pepsi puszka",
+"Pepsi Bottle":"Pepsi butelka",
+"Non-Alcoholic Beer":"Piwo bezalkoholowe"
+   
 },
 
 
